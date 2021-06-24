3b1431cf493781081ce74dc485b4e2bc2f305906
whildan fajar <whildanfajarhidayat93@gmail.com>
2021-06-24 23:48:32 +0700

    ActivityPAM11

---
+++ app/src/main/java/com/example/activity9pam/BarangEdit.java
@@ -0,0 +1,63 @@
+package com.example.activity9pam;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.activity9pam.database.barang;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class BarangEdit extends AppCompatActivity {
+    TextView tv_Key;
+    EditText ed_Nama;
+    Button btnEdit;
+    DatabaseReference databaseReference;
+    String kode, nama, key;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barang_edit);
+
+        tv_Key = findViewById(R.id.tv_key);
+        ed_Nama = findViewById(R.id.edNama);
+        btnEdit = findViewById(R.id.btEdit);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        Bundle bundle = this.getIntent().getExtras();
+        kode = bundle.getString("Kunci1");
+        nama = bundle.getString("Kunci2");
+        key = bundle.getString("Kunci3");
+
+        tv_Key.setText(key);
+        ed_Nama.setText(nama);
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editBarang(new barang(kode, ed_Nama.getText().toString()));
+            }
+        });
+    }
+
+    public void editBarang(barang barang) {
+        databaseReference.child("Barang")
+                .child(key)
+                .setValue(barang)
+                .addOnSuccessListener(this, new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(BarangEdit.this, "Data Berhasil diupdate", Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                });
+    }
+}
--- app/src/main/java/com/example/activity9pam/adapter/AdapterBarang.java
+++ app/src/main/java/com/example/activity9pam/adapter/AdapterBarang.java
@@ -1,80 +1,136 @@
 package com.example.activity9pam.adapter;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.content.Context;
+import android.widget.Toast;
+
 import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.activity9pam.BarangEdit;
+import com.example.activity9pam.MainActivity;
 import com.example.activity9pam.R;
 import com.example.activity9pam.database.barang;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AdapterBarang extends
         RecyclerView.Adapter<AdapterBarang.ViewHolder> {
     private ArrayList<barang> daftarBarang;
     private Context context;
+    private DatabaseReference databaseReference;
+
     public AdapterBarang(ArrayList<barang> barangs, Context ctx){
-        /**
-         * Inisiasi data dan variabel yang akan digunakan
-         */
+
         daftarBarang = barangs;
         context = ctx;
     }
     class ViewHolder extends RecyclerView.ViewHolder {
-        /**
-         * Inisiasi View
-         * Disini kita hanya menggunakan data String untuk tiap item
-         * dan juga view nya hanyalah satu TextView
-         */
+
         TextView tvTitle;
         ViewHolder(View v) {
             super(v);
             tvTitle = (TextView) v.findViewById(R.id.tv_barang);
+
+            databaseReference = FirebaseDatabase.getInstance().getReference();
         }
     }
+
+    @NonNull
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        /**
-         * Inisiasi ViewHolder
-         */
-        View v =
-                LayoutInflater.from(parent.getContext()).inflate(R.layout.item_barang, parent,
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_barang, parent,
                         false);
         // mengeset ukuran view, margin, padding, dan parameter layout lainnya
         ViewHolder vh = new ViewHolder(v);
         return vh;
-    } @Override
-    public void onBindViewHolder(ViewHolder holder, final int position) {
-        /**
-         * Menampilkan data pada view
-         */
+    }
+    @Override
+    public void onBindViewHolder(@NonNull AdapterBarang.ViewHolder holder, final int position) {
+        String kode, nama, key;
+        kode = daftarBarang.get(position).getKode();
+        nama = daftarBarang.get(position).getNama();
+        key = daftarBarang.get(position).getKode();
+
         final String name = daftarBarang.get(position).getNama();
         holder.tvTitle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /**
-                 * untuk latihan Selanjutnya , jika ingin membaca detail data
-                 */
             }
         });
         holder.tvTitle.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                /**
-                 * untuk latihan Selanjutnya ,fungsi Delete dan Update data
-                 */
-                return true;
-            }
-        });
+                                                  @Override
+                                                  public boolean onLongClick(View view) {
+                                                      PopupMenu popupMenu = new PopupMenu(view.getContext(), view);
+                                                      popupMenu.inflate(R.menu.menubarang);
+                                                      popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                                                          @Override
+                                                          public boolean onMenuItemClick(MenuItem menuItem) {
+                                                              switch (menuItem.getItemId()) {
+                                                                  case R.id.mnEdit:
+                                                                      Bundle bundle = new Bundle();
+                                                                      bundle.putString("Kunci1", kode);
+                                                                      bundle.putString("Kunci2", nama);
+                                                                      bundle.putString("Kunci3", key);
+
+                                                                      Intent intent = new Intent(view.getContext(), BarangEdit.class);
+                                                                      intent.putExtras(bundle);
+                                                                      view.getContext().startActivity(intent);
+                                                                      break;
+                                                                  case R.id.mnHapus:
+                                                                      AlertDialog.Builder alertDialog = new AlertDialog.Builder(view.getContext());
+                                                                      alertDialog.setTitle("Yakin data " + nama + " akan dihapus?");
+                                                                      alertDialog.setMessage("Tekan 'Ya' untuk menghapus")
+                                                                              .setCancelable(false)
+                                                                              .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                                                                                  @Override
+                                                                                  public void onClick(DialogInterface dialogInterface, int i) {
+                                                                                      deleteData(key);
+                                                                                      Toast.makeText(view.getContext(), "Data " + nama + " berhasil dihapus", Toast.LENGTH_LONG).show();
+                                                                                      Intent intent1 = new Intent(view.getContext(), MainActivity.class);
+                                                                                      view.getContext().startActivity(intent1);
+                                                                                  }
+                                                                              })
+                                                                              .setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                                                                                  @Override
+                                                                                  public void onClick(DialogInterface dialogInterface, int i) {
+                                                                                      dialogInterface.cancel();
+                                                                                  }
+                                                                              });
+                                                                      AlertDialog dialog = alertDialog.create();
+                                                                      dialog.show();
+                                                                      break;
+                                                              }
+
+                                                              return true;
+                                                          }
+                                                      });
+                                                      popupMenu.show();
+                                                      return true;
+                                                  }
+                                              });
         holder.tvTitle.setText(name);
     }
+
+    public void deleteData(String key) {
+                if (databaseReference != null) {
+                    databaseReference.child("Barang").child(key).removeValue();
+                }
+            }
+
     @Override
     public int getItemCount() {
-        /**
-         * Mengembalikan jumlah item pada barang
-         */
         return daftarBarang.size();
     }
 }
